ff89e80bee87448ea2d434ac33d8ef901935ddca
Henry-Cho <hyungseok5926@gmail.com>
2021-04-26 13:18:10 +0900

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import './static/App.css';
-import { ConnectedRouter } from 'connected-react-router';
-import { BrowserRouter, Route } from 'react-router-dom';
-import {history} from './redux/configStore';
-import {useDispatch} from 'react-redux';
-import {setUser} from './redux/modules/user';
-import {api as userActions} from './redux/modules/user';
+import React from "react";
+import "./static/App.css";
+import { ConnectedRouter } from "connected-react-router";
+import { BrowserRouter, Route } from "react-router-dom";
+import { history } from "./redux/configStore";
+import { useDispatch } from "react-redux";
+import { setUser } from "./redux/modules/user";
+import { api as userActions } from "./redux/modules/user";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const dispatch = useDispatch()
-  
-  // 예시입니다. 
+  const dispatch = useDispatch();
+
+  // 예시입니다.
   React.useEffect(() => {
-    dispatch(userActions.exDispatch('유저정보'))
-  },[])
+    dispatch(userActions.exDispatch("유저정보"));
+  }, []);
   return (
     <React.Fragment>
       <BrowserRouter>
-      <ConnectedRouter history={history}>
-        <div>이거 지우고 시작</div>
-      </ConnectedRouter>
+        <Sidebar />
+        <ConnectedRouter history={history}></ConnectedRouter>
       </BrowserRouter>
     </React.Fragment>
   );
