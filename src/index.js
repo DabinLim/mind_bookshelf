976de4e2f14ea69f3284e71d0083181fb79b9abf
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-04-27 15:41:55 +0900

    프로필 수정 기능

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './static/index.css';
+import store from './redux/configStore';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './redux/configStore';
 
 ReactDOM.render(
   <Provider store={store}>
