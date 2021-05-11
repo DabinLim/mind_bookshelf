474b37fc5abd097c1adf8bab868759c4c27a0c26
Henry-Cho <hyungseok5926@gmail.com>
2021-05-11 14:17:05 +0900

    font changed

--- src/components/Community/CommunityQnA.js
+++ src/components/Community/CommunityQnA.js
@@ -14,17 +14,13 @@ const CommunityQnA = (props) => {
   const dispatch = useDispatch();
   const [cardModal, setCardModal] = useState(false);
   const user = useSelector((state) => state.user.user);
-  console.log(props.topic);
-  console.log(cardModal);
   const closeCardModal = () => {
-    console.log("close");
     setCardModal(false);
   };
 
   const openCard = (a) => {
     const type = "community";
     setCardModal(true);
-    console.log("open");
     dispatch(communityActions.getCardDetail(a.answerId, type));
     dispatch(commentActions.getCommentAX(a.answerId));
   };
