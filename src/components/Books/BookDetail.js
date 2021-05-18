0e206171cd867aedccdb74d28636b1c09cb747cf
Dabin <dabinim09@gmail.com>
2021-05-18 19:17:33 +0900

    todaycards in bookdetail

--- src/components/Books/BookDetail.js
+++ src/components/Books/BookDetail.js
@@ -665,8 +665,9 @@ const Date = styled.div`
 const Cards = styled.div`
   display:flex;
   flex-direction:column;
-  justify-content:flex-end;
+  justify-content:flex-start;
   width:100%;
+  min-height:80px;
 `;
 
 const Selected = styled.span`
