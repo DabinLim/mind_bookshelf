b10f8b2d70669d83fec237ce811a01ce06abee8f
Daeho Lee <67696504+leedaeho1188@users.noreply.github.com>
2021-05-24 21:30:11 +0900

    corrected when not login

--- src/components/Community/QuestionDetail.js
+++ src/components/Community/QuestionDetail.js
@@ -17,6 +17,7 @@ import { api as commentActions } from "../../redux/modules/comment";
 import { api as communityActions,resetAll as LikeReset } from "../../redux/modules/community";
 import {history} from '../../redux/configStore';
 import {CheckOutlined} from "@ant-design/icons";
+import GoBack from '../../elements/GoBack'
 
 const QuestionDetail = (props) => {
   const dispatch = useDispatch();
@@ -104,6 +105,9 @@ const QuestionDetail = (props) => {
         {cardModal ? <CardModal close={closeCardModal} /> : null}
         <CommunityContainer>
           <Container>
+            <div style={{margin:"0px 0px 29px 20px"}}>
+              <GoBack/>
+            </div>
             <ContainerUpper>
               <ContainerUpperLeft>
                 <HashTag
@@ -460,7 +464,7 @@ const Container = styled.section`
   }
   @media (max-width: 500px) {
     width: 360px;
-    padding: 50px 0 30px 0;
+    padding: 25px 0 30px 0;
   }
 `;
 
@@ -518,13 +522,11 @@ const HashTag = styled.div`
 const FilterBtnBoxMobile = styled.div`
   width: 40%;
   display: flex;
-  margin-top: 14px;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-end;
   font-size: 12px;
   margin-right: 20px;
-  padding-top: 35px;
   box-sizing: border-box;
   @media (min-width: 650px) {
     display: none;
